960c96d95c5b71727e71f8edd95b447fbedaf970
roopal92 <bansalroo06@gmail.com>
2020-05-04 23:48:06 +0530

    home component n contact us added

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 import Menu from './MenuComponent';
 import { DISHES } from '../shared/dishes';
-import DishDetail from './DishDetailComponent';
+// import DishDetail from './DishDetailComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import Home from './HomeComponent';
 import { Switch, Route, Redirect } from 'react-router-dom';
+import Contact from './ContactComponent';
+import {LEADERS} from '../shared/leaders';
+import {PROMOTIONS} from '../shared/promotions';
+import {COMMENTS} from '../shared/comments';
 
 class Main extends Component {
 
@@ -14,6 +18,9 @@ class Main extends Component {
 
         this.state = {
             dishes: DISHES,
+            comments: COMMENTS,
+            promotions: PROMOTIONS,
+            leaders: LEADERS
             /* selectedDish: null,*/
         };
     }
@@ -25,7 +32,9 @@ class Main extends Component {
     render() {
         const HomePage=()=>{
             return (
-                <Home />
+                <Home dish={this.state.dishes.filter((dish)=> dish.featured)[0]}
+                promotion={this.state.promotions.filter((promo)=> promo.featured)[0]}
+                leader={this.state.leaders.filter((leader)=> leader.featured)[0]}/>
             );
         }
         return (
@@ -37,6 +46,7 @@ class Main extends Component {
                 <Switch>
                     <Route path="/home" component={HomePage}></Route>
                     <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes}/>} />
+                    <Route exact path="/contactus" component={Contact}  />
                     <Redirect to="/home" />
                 </Switch>
                 <Footer />
--- src/shared/dishes.js
+++ src/shared/dishes.js
@@ -1,71 +1,43 @@
-export const DISHES=[
-    {
+export const DISHES =
+    [
+        {
         id: 0,
         name:'Uthappizza',
-        image: 'assets/images/uthappizza.png',
+        image: '/assets/images/uthappizza.png',
         category: 'mains',
         label:'Hot',
         price:'4.99',
-        description:'A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.',              
-        comments: [
-         {
-             comment: 'waoo',
-             author: 'Chandeler',
-             date: 'Oct 27, 2019'
-         },
-         {
-             comment: 'amazing',
-             author: 'Jay',
-             date: 'Oct 22, 2019'
-         },
-         {
-             comment: 'great',
-             author: 'joe',
-             date: 'Oct 21, 2019'
-         }
-     ]  
-   },
-     {
+        featured: true,
+        description:'A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.'                    
+        },
+        {
         id: 1,
         name:'Zucchipakoda',
-        image: 'assets/images/zucchipakoda.png',
+        image: '/assets/images/zucchipakoda.png',
         category: 'appetizer',
         label:'',
         price:'1.99',
-        comments: [
-         {
-             comment: 'okish',
-             author: 'Monica',
-             date: 'Oct 27, 2019'
-         },
-         {
-             comment: 'awesome',
-             author: 'Roopal',
-             date: 'Aug 27, 2012'
-         },
-         {
-             comment: 'fsdfndnfdsnffkm',
-             author: 'Rachel',
-             date: 'May 27, 2010'
-         }
-     ],
-        description:'Deep fried Zucchini coated with mildly spiced Chickpea flour batter accompanied with a sweet-tangy tamarind sauce'                        },
-     {
+        featured: false,
+        description:'Deep fried Zucchini coated with mildly spiced Chickpea flour batter accompanied with a sweet-tangy tamarind sauce'
+        },
+        {
         id: 2,
         name:'Vadonut',
-        image: 'assets/images/vadonut.png',
+        image: '/assets/images/vadonut.png',
         category: 'appetizer',
         label:'New',
         price:'1.99',
-        description:'A quintessential ConFusion experience, is it a vada or is it a donut?'                        },
-     {
+        featured: false,
+        description:'A quintessential ConFusion experience, is it a vada or is it a donut?'
+        },
+        {
         id: 3,
         name:'ElaiCheese Cake',
-        image: 'assets/images/elaicheesecake.png',
+        image: '/assets/images/elaicheesecake.png',
         category: 'dessert',
         label:'',
         price:'2.99',
-        description:'A delectable, semi-sweet New York Style Cheese Cake, with Graham cracker crust and spiced with Indian cardamoms'                        
-    }
-     
+        featured: false,
+        description:'A delectable, semi-sweet New York Style Cheese Cake, with Graham cracker crust and spiced with Indian cardamoms'
+        }
     ];
